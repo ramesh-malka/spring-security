a149b16b72dbbf85a9bab66ae167a7305ced82f5
rameshmalka <rameshmalka1993@gmail.com>
2023-04-21 12:37:56 -0400

    Add Custom user details service - Section 3, lec 19

--- src/main/java/com.ramesh.spring.security/MyCustomSecurityConfig.java
+++ src/main/java/com.ramesh.spring.security/MyCustomSecurityConfig.java
@@ -3,12 +3,32 @@ package com.ramesh.spring.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Configuration
 public class MyCustomSecurityConfig {
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("tom")
+                .password(passwordEncoder().encode("cruise"))
+                .authorities("read").build();
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
