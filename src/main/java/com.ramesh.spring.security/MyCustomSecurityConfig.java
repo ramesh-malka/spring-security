9e6a703fd949bae3deeb0106da6fc76536e985c2
Ramesh Malka <rameshmalka@Rameshs-MacBook-Pro.local>
2023-04-21 12:15:33 -0400

    Initial commit and security with springboot 2.0

---
+++ src/main/java/com.ramesh.spring.security/MyCustomSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.ramesh.spring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class MyCustomSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.httpBasic();
+        httpSecurity.authorizeHttpRequests().requestMatchers("/hello").authenticated();
+        httpSecurity.addFilterBefore(new MySecurityFilter(), BasicAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+}
